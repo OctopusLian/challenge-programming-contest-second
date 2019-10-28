bb56cfe28aeeaf2da79215ab2e5d263b9efa9c40
OctopusLian <zoctopus@qq.com>
2019-10-28 22:03:15 +0800

    完成ASDS1_4_C-有bug and D

--- ch05/ALDS1_4_C_Dictionary.c
+++ ch05/ALDS1_4_C_Dictionary.c
@@ -6,8 +6,69 @@
 #define L 14
 
 // 将字符转换为数值
-
+int getChar(char ch) {
+    if (ch == 'A') return 1;
+    else if (ch == 'C') return 2;
+    else if (ch == 'G') return 3;
+    else if (ch == 'T') return 4;
+    else return 0;
+}
 
 // 将字符串转换为数值并生成key
+long long getKey(char str[]) {
+    long long sum = 0,p = 1,i;
+    for (i = 0;i < strlen(str);i++) {
+        sum += p*(getChar(str[i]));
+        p *= 5;
+    }
+    return sum;
+}
 
+int h1(int key) {return key % M;}
+int h2(int key) {return 1 + (key % (M - 1));}
 
+int find(char str[]) {
+    long long key,i,h;
+    key = getKey(str);  //将字符串转换为数值
+    for (i = 0;;i++) {
+        h = (h1(key) + i * h2(key)) % M;
+        if (strcmp(H[h],str) == 0) return 1;
+        else if (strlen(H[h]) == 0) return 0;
+    }
+    return 0;
+}
+
+int insert(char str[]) {
+    long long key,i,h;
+    key = getKey(str);  //将字符串换成数值
+    for(i = 0; ;i++) {
+        h = (h1(key) + i * h2(key)) % M;
+        if (strcmp(H[h],str) == 0) return 1;
+        else if (strlen(H[h]) == 0) {
+            strcpy(H[h],str);
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int i,n,h;
+    char str[L],com[9];
+    for(i = 0;i < M;i++) H[i][0] = '\0';
+    scanf("%d",&n);
+    for(i = 0;i < n;i++) {
+        scanf("%s %s",com,str);
+
+        if (com[0] == '1') {
+            insert(str);
+        } else {
+            if (find(str)) {
+                printf("yes\n");
+            } else {
+                printf("no\n")
+            }
+        }
+    }
+    return 0;
+}
--- ch05/ALDS1_4_D_Allocation.cpp
+++ ch05/ALDS1_4_D_Allocation.cpp
@@ -3,7 +3,39 @@ using namespace std;
 #define MAX 100000
 typedef long long llong;
 
-int n,l;
+int n,k;
 llong T[MAX];
 
 // k辆最大运载量为P的卡车能装多少货物
+int check(llong P) {
+    int i = 0;
+    for(int j = 0; j < k; j++) {
+        llong s = 0;
+        while(s + T[i] <= P) {
+            s += T[i];
+            i++;
+            if (i == n) return n;
+        }
+    }
+    return i;
+}
+
+int solve() {
+    llong left = 0;
+    llong right = 100000 * 10000; //货物数 x 1 件货物的最大重量
+    llong mid;
+    while(right - left > 1) {
+        mid = (left + right) / 2;
+        int v = check(mid); //mid == 检查 mid == P 时能装多少货物
+        if (v >= n) right = mid;
+        else left = mid;
+    }
+    return right;
+}
+
+main() {
+    cin >> n >> k;
+    for(int i = 0;i < n;i++) cin >> T[i];
+    llong ans = solve();
+    cout << ans << endl;
+}
